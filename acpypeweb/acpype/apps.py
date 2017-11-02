79b4ecf7f32c7996fdd4a5173be09ece42bfe814
lkagami <luciano.kagami@ufrgs.br>
2017-11-02 18:22:39 -0200

    Remove acpypeweb folder

--- acpypeweb/acpype/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AcpypeConfig(AppConfig):
-    name = 'acpype'
--- acpypeweb/acpype/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
