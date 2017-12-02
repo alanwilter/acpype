d21ccd22241cb5cd1dca1e3d64bc80f243a13dbb
lkagami <luciano.kagami@ufrgs.br>
2017-12-02 12:00:48 -0200

    Upgrade - Status function + download

---
+++ acpypeserver/submit/forms.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.forms import AuthenticationForm 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username", max_length=30, 
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+    password = forms.CharField(label="Password", max_length=30, 
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
+
+class SubmitionForm(forms.Form):
+
+    molecule_file = forms.FileField(help_text='max. 1 megabyte', required=False)
+    charge_method = forms.CharField(required=False)
+    net_charge = forms.IntegerField(required=False)
+    multiplicity = forms.IntegerField(required=False)
+    atom_type = forms.CharField(required=False)
---
+++ acpypeserver/submit/tasks.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import, unicode_literals
+import os
+import shutil
+from acpypeserver import settings
+from celery import shared_task
+import MySQLdb
+import django.contrib.auth
+from .models import Submition
+from django.contrib.auth import login, authenticate
+import os.path
+from time import gmtime, strftime
+
+@shared_task(ignore_result=False)
+def process(user_name,cm,nc,ml,at,mfs):
+	dt = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
+	name_file = ((str(mfs)).split('.')[0])
+	media_dir=os.chdir(settings.MEDIA_ROOT)
+	os.chdir(settings.MEDIA_ROOT)
+	execute_acpype = 'acpype -c {} -n {} -m {} -a {} -i {}'.format(cm,nc,ml,at,mfs)
+	os.system(execute_acpype)
+	output_filename = '{}_acpype_{}'.format(name_file,dt)
+	dir_name = '{}.acpype'.format(name_file)
+	shutil.make_archive(output_filename, 'zip', dir_name)
+	job = Submition.objects.filter(juser=user_name).get(jstatus="STARTED")
+	job.jstatus = "DONE"
+	job.jzipped = output_filename
+	job.save()
+	try:
+		os.path.isfile(mfs)
+		os.remove(mfs)
+	except:
+		pass
