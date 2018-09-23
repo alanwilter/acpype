355ac6bda107beb23a96d20b7dca85e576ff7091
lkagami <luciano.kagami@ufrgs.br>
2018-09-22 21:18:38 -0300

    Delete serializers.py

--- acpypeserver/submit/serializers.py
+++
@@ -1,8 +0,0 @@
-from submit.models import Submission
-from rest_framework import serializers
-
-
-class SubmissionSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Submission
-        fields = ('molecule_file', 'charge_method', 'net_charge', 'multiplicity', 'atom_type', 'juser', 'jstatus', 'jzipped', 'jlog', 'date', 'usr_folder')
