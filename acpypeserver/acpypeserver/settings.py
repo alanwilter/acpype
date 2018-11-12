aac7d19eead351cb89e71cdb140e60f9a28c3d28
lkagami <luciano.kagami@ufrgs.br>
2018-11-12 18:32:24 -0200

    Run number limited

--- acpypeserver/acpypeserver/settings.py
+++ acpypeserver/acpypeserver/settings.py
@@ -124,6 +124,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CELERY_RESULT_BACKEND = 'db+mysql://acpype_mysql:@Cpype18@localhost/acpype_db'
 CELERY_TIMEZONE = TIME_ZONE
+CELERYD_PREFETCH_MULTIPLIER = 1
 LOGIN_REDIRECT_URL = ('..')
 LOGIN_URL = ('login')
 LOGOUT_REDIRECT_URL = ('home')
