9a1b1b1476d8e9f34109892f0c7fa9cbe2465ce6
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2019-07-02 14:44:55 +0100

    Updated references and citation

--- acpype/acpype.py
+++ acpype/acpype.py
@@ -22,26 +22,30 @@
 
     - xplo2d (G.J. Kleywegt)
 
-    For Antechamber, please cite:
-    1.  Wang, J., Wang, W., Kollman P. A.; Case, D. A. "Automatic atom type and
-        bond type perception in molecular mechanical calculations". Journal of
-        Molecular Graphics and Modelling , 25, 2006, 247260.
-    2.  Wang, J., Wolf, R. M.; Caldwell, J. W.; Kollman, P. A.; Case, D. A.
-        "Development and testing of a general AMBER force field". Journal of
-        Computational Chemistry, 25, 2004, 1157-1174.
-
     For Non-uniform 1-4 scale factor conversion (e.g. if using GLYCAM06), please cite:
 
-    Austen Bernardi, Roland Faller, Dirk Reith, Karl N. Kirschner, "Conversion of
-    GLYCAM06 Force-Field Parameters from AMBER to GROMACS Topologies"
-    submitted for review, April 2018."
+    BERNARDI, A., FALLER, R., REITH, D., and KIRSCHNER, K. N. ACPYPE update for
+    nonuniform 1–4 scale factors: Conversion of the GLYCAM06 force field from AMBER
+    to GROMACS. SoftwareX 10 (2019), 100241. doi: 10.1016/j.softx.2019.100241
+
+    For Antechamber, please cite:
+    1. WANG, J., WANG, W., KOLLMAN, P. A., and CASE, D. A. Automatic atom type and
+       bond type perception in molecular mechanical calculations. Journal of Molecular
+       Graphics and Modelling 25, 2 (2006), 247–260. doi: 10.1016/j.jmgm.2005.12.005
+    2. WANG, J., WOLF, R. M., CALDWELL, J. W., KOLLMAN, P. A., and CASE, D. A.
+       Development and testing of a General Amber Force Field. Journal of Computational
+       Chemistry 25, 9 (2004), 1157–1174. doi: 10.1002/jcc.20035
 
     If you use this code, I am glad if you cite:
 
     SOUSA DA SILVA, A. W. & VRANKEN, W. F.
     ACPYPE - AnteChamber PYthon Parser interfacE.
-    BMC Research Notes 2012, 5:367 doi:10.1186/1756-0500-5-367
+    BMC Research Notes 5 (2012), 367 doi: 10.1186/1756-0500-5-367
     http://www.biomedcentral.com/1756-0500/5/367
+
+    BATISTA, P. R.; WILTER, A.; DURHAM, E. H. A. B. & PASCUTTI, P. G. Molecular
+    Dynamics Simulations Applied to the Study of Subtypes of HIV-1 Protease.
+    Cell Biochemistry and Biophysics 44 (2006), 395-404. doi: 10.1385/CBB:44:3:395
 
     Alan Wilter Sousa da Silva, D.Sc.
     Bioinformatician, UniProt, EMBL-EBI
@@ -2755,12 +2759,12 @@ Usage: antechamber -i  input file name
                 c0, c1, c2, c3, c4, c5 = dih[1]
                 line = \
                     "%6i %6i %6i %6i %6i %10.5f %10.5f %10.5f %10.5f %10.5f %10.5f" % \
-                    (id1, id2, id3, id4, 3, c0, c1, c2, c3, c4, c5) \
-                    +" ; %6s-%6s-%6s-%6s\n" % (a1, a2, a3, a4)
+                    (id1, id2, id3, id4, 3, c0, c1, c2, c3, c4, c5) + " ; %6s-%6s-%6s-%6s\n" \
+                    % (a1, a2, a3, a4)
                 oline = \
                     "%6i %6i %6i %6i %6i ; %10.5f %10.5f %10.5f %10.5f %10.5f %10.5f" % \
-                    (id1, id2, id3, id4, 3, c0, c1, c2, c3, c4, c5) \
-                    +" ; %6s-%6s-%6s-%6s    %4s-%4s-%4s-%4s\n" % (a1, a2, a3, a4, oat1, oat2, oat3, oat4)
+                    (id1, id2, id3, id4, 3, c0, c1, c2, c3, c4, c5) + " ; %6s-%6s-%6s-%6s    %4s-%4s-%4s-%4s\n" \
+                    % (a1, a2, a3, a4, oat1, oat2, oat3, oat4)
                 temp.append(line)
                 otemp.append(oline)
             temp.sort()
@@ -2904,9 +2908,10 @@ Usage: antechamber -i  input file name
                 topText.append(" %-16s %-6i\n" % ('WAT', nWat))
 
         if self.topo14Data.hasNondefault14():
-            citation = ('     Austen Bernardi, Roland Faller, Dirk Reith, Karl N. Kirschner, "Conversion of\n' +
-                        '     GLYCAM06 Force-Field Parameters from AMBER to GROMACS Topologies"\n' +
-                        '     submitted for review, April 2018."\n')
+            citation = ('     BERNARDI, A., FALLER, R., REITH, D., and KIRSCHNER, K. N. ACPYPE update for\n' +
+                        '     nonuniform 1–4 scale factors: Conversion of the GLYCAM06 force field from AMBER\n' +
+                        '     to GROMACS. SoftwareX 10 (2019), 100241. doi: 10.1016/j.softx.2019.100241"\n')
+
             msg = ('Non-default 1-4 scale parameters detected.  Converting individually. Please cite:\n\n' +
                    citation)
 
@@ -3476,7 +3481,7 @@ class MolTopol(ACTopol):
 #             self.printWarn("Consider installing http://openbabel.org")
 
         self.xyzFileData = open(acFileXyz, 'r').readlines()
-        self.topFileData = [ x for x in open(acFileTop, 'r').readlines() if not x.startswith('%COMMENT') ]
+        self.topFileData = [x for x in open(acFileTop, 'r').readlines() if not x.startswith('%COMMENT')]
         self.topo14Data = Topology_14()
         self.topo14Data.read_amber_topology(''.join(self.topFileData))
         self.printDebug("prmtop and inpcrd files loaded")
@@ -3718,7 +3723,7 @@ def init_main():
             molecule.createMolTopol()
         acpypeFailed = False
     except Exception:
-        exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+        _exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
         print("ACPYPE FAILED: %s" % exceptionValue)
         if args.debug:
             traceback.print_tb(exceptionTraceback, file=sys.stdout)
@@ -3750,5 +3755,6 @@ def init_main():
     except Exception:
         pass
 
+
 if __name__ == '__main__':
-    init_main()                # necessary for to call in anaconda package;
+    init_main()  # necessary for to call in anaconda package;
