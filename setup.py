8b749820044a51b267bb8657b1413766185dc29f
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2019-10-07 23:07:26 +0200

    To include 3rd party libs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
     long_description = fh.read()
 
-__updated__ = "2019-10-07T23:00:59CEST"
+__updated__ = "2019-10-07T23:20:34CEST"
 version = __updated__[:19].replace('-', '').replace('T', '').replace(':', '')
 
 setuptools.setup(
