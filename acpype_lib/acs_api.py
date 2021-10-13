d503bcc6d0a420e670b8a32b90e34f58ed17d10e
lkagami <luciano.kagami@ufrgs.br>
2021-10-13 19:37:44 -0300

    fix scripts using blake

--- acpype_lib/acs_api.py
+++ acpype_lib/acs_api.py
@@ -26,7 +26,29 @@ md_mdp = io.StringIO()
 sqm_in = io.StringIO()
 sqm_out = io.StringIO()
 
-filesInMemory = [(em_mdp, "em.mdp") , (AC_frcmod, "_AC.frcmod") , (AC_inpcrd, "_AC.inpcrd") , (AC_lib, "_AC.lib"), (AC_prmtop, "_AC.prmtop"), (bcc_gaff_mol2, "_bcc_gaff.mol2"), (CHARMM_inp, "_CHARMM.inp"), (CHARMM_prm, "_CHARMM.prm"), (CHARMM_rtf, "_CHARMM.rtf"), (CNS_inp, "_CNS.inp"), (CNS_par, "_CNS.par"), (CNS_top, "_CNS.top"), (GMX_OPLS_itp, "_GMX_OPLS.itp"), (GMX_OPLS_top, "_GMX_OPLS.top"), (GMX_gro, "_GMX.gro"), (GMX_itp, "_GMX.itp"), (GMX_top, "_GMX.top"), (NEW_pdb, "_NEW.pdb"), (md_mdp, "md.mdp"), (sqm_in, "sqm.in"), (sqm_out, "sqm.out")]
+filesInMemory = [
+    (em_mdp, "em.mdp"),
+    (AC_frcmod, "_AC.frcmod"),
+    (AC_inpcrd, "_AC.inpcrd"),
+    (AC_lib, "_AC.lib"),
+    (AC_prmtop, "_AC.prmtop"),
+    (bcc_gaff_mol2, "_bcc_gaff.mol2"),
+    (CHARMM_inp, "_CHARMM.inp"),
+    (CHARMM_prm, "_CHARMM.prm"),
+    (CHARMM_rtf, "_CHARMM.rtf"),
+    (CNS_inp, "_CNS.inp"),
+    (CNS_par, "_CNS.par"),
+    (CNS_top, "_CNS.top"),
+    (GMX_OPLS_itp, "_GMX_OPLS.itp"),
+    (GMX_OPLS_top, "_GMX_OPLS.top"),
+    (GMX_gro, "_GMX.gro"),
+    (GMX_itp, "_GMX.itp"),
+    (GMX_top, "_GMX.top"),
+    (NEW_pdb, "_NEW.pdb"),
+    (md_mdp, "md.mdp"),
+    (sqm_in, "sqm.in"),
+    (sqm_out, "sqm.out"),
+]
 
 
 def clearFileInMemory():
@@ -46,7 +68,29 @@ def readFiles(basename):
             files[0].write(line)
 
 
-def acpype_api(inputFile, chargeType="bcc", chargeVal=None, multiplicity="1", atomType="gaff", force=False, basename=None, debug=False, outTopol="all", engine="tleap", allhdg=False, timeTol=MAXTIME, qprog="sqm", ekFlag=None, verbose=True, gmx4=False, disam=False, direct=False, is_sorted=False, chiral=False, is_smiles=False):
+def acpype_api(
+    inputFile,
+    chargeType="bcc",
+    chargeVal=None,
+    multiplicity="1",
+    atomType="gaff",
+    force=False,
+    basename=None,
+    debug=False,
+    outTopol="all",
+    engine="tleap",
+    allhdg=False,
+    timeTol=MAXTIME,
+    qprog="sqm",
+    ekFlag=None,
+    verbose=True,
+    gmx4=False,
+    disam=False,
+    direct=False,
+    is_sorted=False,
+    chiral=False,
+    is_smiles=False,
+):
 
     at0 = time.time()
     print(header)
@@ -55,7 +99,28 @@ def acpype_api(inputFile, chargeType="bcc", chargeVal=None, multiplicity="1", at
         texta = "Python Version %s" % sys.version
         print("DEBUG: %s" % while_replace(texta))
     try:
-        molecule = ACTopol(inputFile=inputFile, chargeType=chargeType, chargeVal=chargeVal, debug=debug, multiplicity=multiplicity, atomType=atomType, force=force, outTopol=outTopol, engine=engine, allhdg=allhdg, basename=basename, timeTol=timeTol, qprog=qprog, ekFlag=ekFlag, verbose=verbose, gmx4=gmx4, disam=disam, direct=direct, is_sorted=is_sorted, chiral=chiral)
+        molecule = ACTopol(
+            inputFile=inputFile,
+            chargeType=chargeType,
+            chargeVal=chargeVal,
+            debug=debug,
+            multiplicity=multiplicity,
+            atomType=atomType,
+            force=force,
+            outTopol=outTopol,
+            engine=engine,
+            allhdg=allhdg,
+            basename=basename,
+            timeTol=timeTol,
+            qprog=qprog,
+            ekFlag=ekFlag,
+            verbose=verbose,
+            gmx4=gmx4,
+            disam=disam,
+            direct=direct,
+            is_sorted=is_sorted,
+            chiral=chiral,
+        )
         molecule.createACTopol()
         molecule.createMolTopol()
         if not basename:
@@ -66,28 +131,29 @@ def acpype_api(inputFile, chargeType="bcc", chargeVal=None, multiplicity="1", at
         "Output in JSON format"
         readFiles(file_name)
         output = {
-            "file_name" : file_name,
-            "em_mdp" : em_mdp.getvalue(),
-            "AC_frcmod" : AC_frcmod.getvalue(),
-            "AC_inpcrd" : AC_inpcrd.getvalue(),
-            "AC_lib" : AC_lib.getvalue(),
-            "AC_prmtop" : AC_prmtop.getvalue(),
-            "bcc_gaff_mol2" : bcc_gaff_mol2.getvalue(),
-            "CHARMM_inp" : CHARMM_inp.getvalue(),
-            "CHARMM_prm" : CHARMM_prm.getvalue(),
-            "CHARMM_rtf" : CHARMM_rtf.getvalue(),
-            "CNS_inp" : CNS_inp.getvalue(),
-            "CNS_par" : CNS_par.getvalue(),
-            "CNS_top" : CNS_top.getvalue(),
-            "GMX_OPLS_itp" : GMX_OPLS_itp.getvalue(),
-            "GMX_OPLS_top" : GMX_OPLS_top.getvalue(),
-            "GMX_gro" : GMX_gro.getvalue(),
-            "GMX_itp" : GMX_itp.getvalue(),
-            "GMX_top" : GMX_top.getvalue(),
-            "NEW_pdb" : NEW_pdb.getvalue(),
-            "md_mdp" : md_mdp.getvalue(),
-            "sqm_in" : sqm_in.getvalue(),
-            "sqm_out" : sqm_out.getvalue()}
+            "file_name": file_name,
+            "em_mdp": em_mdp.getvalue(),
+            "AC_frcmod": AC_frcmod.getvalue(),
+            "AC_inpcrd": AC_inpcrd.getvalue(),
+            "AC_lib": AC_lib.getvalue(),
+            "AC_prmtop": AC_prmtop.getvalue(),
+            "bcc_gaff_mol2": bcc_gaff_mol2.getvalue(),
+            "CHARMM_inp": CHARMM_inp.getvalue(),
+            "CHARMM_prm": CHARMM_prm.getvalue(),
+            "CHARMM_rtf": CHARMM_rtf.getvalue(),
+            "CNS_inp": CNS_inp.getvalue(),
+            "CNS_par": CNS_par.getvalue(),
+            "CNS_top": CNS_top.getvalue(),
+            "GMX_OPLS_itp": GMX_OPLS_itp.getvalue(),
+            "GMX_OPLS_top": GMX_OPLS_top.getvalue(),
+            "GMX_gro": GMX_gro.getvalue(),
+            "GMX_itp": GMX_itp.getvalue(),
+            "GMX_top": GMX_top.getvalue(),
+            "NEW_pdb": NEW_pdb.getvalue(),
+            "md_mdp": md_mdp.getvalue(),
+            "sqm_in": sqm_in.getvalue(),
+            "sqm_out": sqm_out.getvalue(),
+        }
 
     except Exception:
         _exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
