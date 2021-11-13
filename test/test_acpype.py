30ef708e2c7699ecb64d82940a37e08c0b6367ad
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2021-11-13 18:53:28 +0000

    Reorganised tests

--- test/test_acpype.py
+++ test/test_acpype.py
@@ -10,113 +10,56 @@ To run:
 pytest -s --html=report.html
 
 """
-
-import sys
 import os
-import traceback
-import time
-from acpype_lib.acpype import ACTopol, MAXTIME, while_replace, header, elapsedTime
+import shutil
+from acpype_lib.acpype import ACTopol
 
 
-def run_acpype(
-    inputFile,
-    chargeType="gas",
-    chargeVal=None,
-    multiplicity="1",
-    atomType="gaff",
-    force=False,
-    basename=None,
-    debug=False,
-    outTopol="all",
-    engine="tleap",
-    allhdg=False,
-    timeTol=MAXTIME,
-    qprog="sqm",
-    ekFlag=None,
-    verbose=True,
-    gmx4=False,
-    disam=False,
-    direct=False,
-    is_sorted=False,
-    chiral=False,
-    is_smiles=False,
-):
-
-    at0 = time.time()
-    print(header)
-
-    if debug:
-        texta = "Python Version %s" % sys.version
-        print("DEBUG: %s" % while_replace(texta))
-    try:
-        molecule = ACTopol(
-            inputFile=inputFile,
-            chargeType=chargeType,
-            chargeVal=chargeVal,
-            debug=debug,
-            multiplicity=multiplicity,
-            atomType=atomType,
-            force=force,
-            outTopol=outTopol,
-            engine=engine,
-            allhdg=allhdg,
-            basename=basename,
-            timeTol=timeTol,
-            qprog=qprog,
-            ekFlag=ekFlag,
-            verbose=verbose,
-            gmx4=gmx4,
-            disam=disam,
-            direct=direct,
-            is_sorted=is_sorted,
-            chiral=chiral,
-        )
-
-        molecule.createACTopol()
-        molecule.createMolTopol()
-        # if not basename:
-        #     file_name = "smiles_molecule"
-        # else:
-        #     file_name = basename
-
-    except Exception:
-        _exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-        print("ACPYPE FAILED: %s" % exceptionValue)
-        if debug:
-            traceback.print_tb(exceptionTraceback, file=sys.stdout)
-        return 1
-
-    execTime = int(round(time.time() - at0))
-    if execTime == 0:
-        amsg = "less than a second"
-    else:
-        amsg = elapsedTime(execTime)
-    print("Total time of execution: %s" % amsg)
-    return 0
-
-
-def acpype_test(input, name):
+def test_mol2():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    exec_acpype = run_acpype(inputFile=input, basename=name)
-    assert exec_acpype == 0
+    molecule = ACTopol("AAA.mol2", chargeType="gas")
+    molecule.createACTopol()
+    molecule.createMolTopol()
+    assert molecule
+    assert len(molecule.molTopol.atoms) == 33
+    assert len(molecule.molTopol.properDihedrals) == 95
+    assert len(molecule.molTopol.improperDihedrals) == 5
+    shutil.rmtree(molecule.absHomeDir)
 
 
-def test_mol2(input="AAA.mol2"):
-    acpype_test(input, name=None)
+def test_pdb():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    molecule = ACTopol("FFF.mol2", chargeType="gas")
+    molecule.createACTopol()
+    molecule.createMolTopol()
+    assert molecule
+    assert len(molecule.molTopol.atoms) == 63
+    assert len(molecule.molTopol.properDihedrals) == 185
+    assert len(molecule.molTopol.improperDihedrals) == 23
+    shutil.rmtree(molecule.absHomeDir)
 
 
-def test_pdb(input="AAA.pdb"):
-    acpype_test(input, name=None)
+def test_charges():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    molecule = ACTopol("KKK.mol2", chargeType="gas")
+    molecule.createACTopol()
+    molecule.createMolTopol()
+    assert molecule
+    assert len(molecule.molTopol.atoms) == 69
+    assert len(molecule.molTopol.properDihedrals) == 215
+    assert len(molecule.molTopol.improperDihedrals) == 5
+    assert len(molecule.molTopol.chiralGroups) == 3
+    assert molecule.chargeVal == "3"
+    shutil.rmtree(molecule.absHomeDir)
 
 
-def test_chiral(input="thalidomide.mol2"):
-    acpype_test(input, name=None)
-
-
-def test_smiles(input="c1ccc2c(c1)C(=O)N(C2=O)C3CCC(=NC3=O)O"):
-    acpype_test(input, name="thalidomide_smiles")
-    try:
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        os.remove("smiles_molecule.mol2")
-    except Exception as ex:
-        print(ex)
+def test_smiles():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    smiles = "c1ccc2c(c1)C(=O)N(C2=O)C3CCC(=NC3=O)O"
+    molecule = ACTopol(smiles, basename="thalidomide_smiles", chargeType="gas")
+    molecule.createACTopol()
+    molecule.createMolTopol()
+    assert molecule
+    assert len(molecule.molTopol.atoms) == 29
+    shutil.rmtree(molecule.absHomeDir)
+    os.remove(molecule.absInputFile)
