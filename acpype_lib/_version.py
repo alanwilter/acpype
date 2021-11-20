aae8be98550548d4edd72e3d763a2e22ca6f179e
lkagami <luciano.kagami@ufrgs.br>
2021-11-20 12:55:27 -0300

    add pbr to _version

--- acpype_lib/_version.py
+++ acpype_lib/_version.py
@@ -1,10 +1,10 @@
 from subprocess import Popen, STDOUT, PIPE
+from pbr.version import VersionInfo
 
 try:
+    version = VersionInfo("acpype").version_string()
+except Exception:
     version = (
         Popen("git describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE)
         .communicate()[0][:-1]
-        .decode()[0:10]
-    )
-except Exception:
-    version = ""
+        .decode()[0:10])
