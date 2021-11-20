2c1942e2f7f482331e795491b455bbab98065fda
lkagami <luciano.kagami@ufrgs.br>
2021-11-20 09:45:36 -0300

    fix black

--- acpype_lib/_version.py
+++ acpype_lib/_version.py
@@ -2,9 +2,7 @@ from subprocess import Popen, STDOUT, PIPE
 
 try:
     version = (
-        Popen("git describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE)
-        .communicate()[0][:-1]
-        .decode()
+        Popen("git describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE).communicate()[0][:-1].decode()
     )
 except Exception:
     version = "2021.11.17"
