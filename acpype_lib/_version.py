0383f9542bdaa2bca141b6e58500588e30f61e4c
lkagami <luciano.kagami@ufrgs.br>
2021-11-21 13:03:12 -0300

    fix flake8

--- acpype_lib/_version.py
+++ acpype_lib/_version.py
@@ -6,7 +6,7 @@ version = (
     Popen("git describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE).communicate()[0][:-1].decode()[0:10]
 )
 
-if not re.match("^\d{4}\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[01])$", version):
+if not re.match(r"^\d{4}\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[01])$", version):
     try:
         from importlib.metadata import version as ver
 
