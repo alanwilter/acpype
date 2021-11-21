e223684d65013e335cd9b13cd0def5b79dd5d10b
lkagami <luciano.kagami@ufrgs.br>
2021-11-21 12:58:28 -0300

    add regex to get version

--- acpype_lib/_version.py
+++ acpype_lib/_version.py
@@ -1,11 +1,12 @@
 from subprocess import Popen, STDOUT, PIPE
+import re
 
 
 version = (
     Popen("git describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE).communicate()[0][:-1].decode()[0:10]
 )
 
-if version == "fatal: not":
+if not re.match("^\d{4}\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[01])$", version):
     try:
         from importlib.metadata import version as ver
 
