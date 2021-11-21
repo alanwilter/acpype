47e7e59f760a31f44cdc509c243f629e36a96344
lkagami <luciano.kagami@ufrgs.br>
2021-11-21 09:12:43 -0300

    fix get acpype version method; add release commit message trigger

--- acpype_lib/_version.py
+++ acpype_lib/_version.py
@@ -1,13 +1,13 @@
 from subprocess import Popen, STDOUT, PIPE
 
 
-try:
-    version = (
-        Popen("git describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE)
-        .communicate()[0][:-1]
-        .decode()[0:10]
-    )
-except Exception:
+version = (
+    Popen("git describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE)
+    .communicate()[0][:-1]
+    .decode()[0:10]
+)
+
+if version == "fatal: not":
     try:
         from importlib.metadata import version as ver
 
