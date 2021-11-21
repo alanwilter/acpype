cb33c2df049100e2623d0f42aa874f00cf9585f7
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2021-11-21 08:53:15 +0000

    Reorganised again test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,41 +11,43 @@ pytest -s --html=report.html
 
 """
 import os
-import pytest
 import ujson as json
 from acpype_lib.acs_api import acpype_api
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-json_output = acpype_api(
-    inputFile="AAA.mol2", chargeType="gas", atomType="gaff2", debug=True, basename="AAA", chiral=False
-)
-jj = json.loads(json_output)
+
+def get_json():
+    json_output = acpype_api(
+        inputFile="AAA.mol2", chargeType="gas", atomType="gaff2", debug=True, basename="AAA", chiral=False
+    )
+    return json.loads(json_output)
 
 
-@pytest.mark.parametrize(
-    ("atype"),
-    [
-        ("file_name"),
-        ("em_mdp"),
-        ("AC_frcmod"),
-        ("AC_inpcrd"),
-        ("AC_lib"),
-        ("AC_prmtop"),
-        ("mol2"),
-        ("CHARMM_inp"),
-        ("CHARMM_prm"),
-        ("CHARMM_rtf"),
-        ("CNS_inp"),
-        ("CNS_par"),
-        ("CNS_top"),
-        ("GMX_OPLS_itp"),
-        ("GMX_OPLS_top"),
-        ("GMX_gro"),
-        ("GMX_itp"),
-        ("GMX_top"),
-        ("NEW_pdb"),
-        ("md_mdp"),
-    ],
+file_types = (
+    ("file_name"),
+    ("em_mdp"),
+    ("AC_frcmod"),
+    ("AC_inpcrd"),
+    ("AC_lib"),
+    ("AC_prmtop"),
+    ("mol2"),
+    ("CHARMM_inp"),
+    ("CHARMM_prm"),
+    ("CHARMM_rtf"),
+    ("CNS_inp"),
+    ("CNS_par"),
+    ("CNS_top"),
+    ("GMX_OPLS_itp"),
+    ("GMX_OPLS_top"),
+    ("GMX_gro"),
+    ("GMX_itp"),
+    ("GMX_top"),
+    ("NEW_pdb"),
+    ("md_mdp"),
 )
-def test_json(atype):
-    assert len(jj.get(atype)) > 2
+
+
+def test_json():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    jj = get_json()
+    for ft in file_types:
+        assert len(jj.get(ft)) > 2
