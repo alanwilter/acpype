2bea3b9e7f4b8b54b3738ae6b6e197838a5bf853
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2021-11-24 15:34:04 +0000

    Use today if anything else fails

--- acpype_lib/_version.py
+++ acpype_lib/_version.py
@@ -1,29 +1,34 @@
 import os
 import re
 from subprocess import run, STDOUT, PIPE
+from importlib.metadata import version as ver
+from datetime import datetime
+
+today = datetime.today().strftime("%Y.%m.%d")
+
+
+def parse_ver(ver):
+    m = re.match(r"(\d{4}.\d{2}.\d{2}(-\d+)?)(\-.*)?", ver)
+    if m:
+        version = m.group(1).replace("-", ".")
+        status = 0
+    else:
+        version = "0"
+        status = 1
+    return version, status
 
 
 def run_git():
     cur_dir = os.getcwd()
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    ver = run("git describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE)
+    ver = run("gits describe --tags --always", shell=True, stderr=STDOUT, stdout=PIPE)
     os.chdir(cur_dir)
-    m = re.match(r"(\d{4}.\d{2}.\d{2}(-\d+)?)(\-.*)", ver.stdout.decode())
-    if m:
-        ver = m.group(1).replace("-", ".")
-        status = 0
-    else:
-        ver = "0"
-        status = 1
-    return ver, status
+    return parse_ver(ver.stdout.decode())
 
 
 version, status = run_git()
 
 if status != 0:
-    try:
-        from importlib.metadata import version as ver
-
-        version = ver("acpype")
-    except Exception:
-        version = "0"
+    version, status = parse_ver(ver("acpype"))
+if status != 0:
+    version = today
