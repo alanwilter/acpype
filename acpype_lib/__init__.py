8bb9b072271fe0deb7a6f43847aa36f0756e9dcf
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2021-12-01 11:31:56 +0000

    Changed cron

--- acpype_lib/__init__.py
+++ acpype_lib/__init__.py
@@ -1,4 +1,4 @@
 # from https://packaging.python.org/guides/single-sourcing-package-version/
 # using option 2
 # updated automatically via pre-commit git-hook
-__version__ = "2021.11.30"
+__version__ = "2021.12.01"
