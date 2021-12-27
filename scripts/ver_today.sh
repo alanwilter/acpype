c253ad6003d96a99942c96f87f2d2a8f3e1c075d
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2021-12-27 11:11:59 +0000

    Fixed ver_today with pcregrep (linux/macos)

--- scripts/ver_today.sh
+++ scripts/ver_today.sh
@@ -7,12 +7,12 @@ while IFS= read -r afile; do
 	echo "Updated version to $today in file $afile"
 	perl -pi -e "s/\b[0-9]{4}\.[12]?[0-9]\.[12]?[0-9]\b/$today/g" "$afile"
 	git add "$afile"
-done < <(grep -lEr "\b\d{4}\.[12]?[0-9]\.[12]?[0-9]\b" -- **/*.py pyproject.toml)
+done < <(pcregrep -lwr "\d{4}\.[12]?[0-9]\.[12]?[0-9]" ./*/*.py pyproject.toml)
 
 while IFS= read -r afile; do
 	echo "Updated last_tag to $last_tag in file $afile"
 	perl -pi -e "s/\b[0-9]{4}\.[12]?[0-9]\.[12]?[0-9]\b/$last_tag/g" "$afile"
 	git add "$afile"
-done < <(grep -lEr "\b\d{4}\.[12]?[0-9]\.[12]?[0-9]\b" -- README.md)
+done < <(pcregrep -lwr "\d{4}\.[12]?[0-9]\.[12]?[0-9]" README.md)
 
 exit 0
