a3fb4254932240bed5b24150928b266c2a6641e1
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2022-01-01 14:09:40 +0100

    Fixed regex in ver_today.sh

--- acpype/__init__.py
+++ acpype/__init__.py
@@ -57,4 +57,4 @@
 # from https://packaging.python.org/guides/single-sourcing-package-version/
 # using option 2
 # updated automatically via pre-commit git-hook
-__version__ = "2021.12.30"
+__version__ = "2022.1.1"
--- scripts/ver_today.sh
+++ scripts/ver_today.sh
@@ -5,14 +5,14 @@ last_tag=$(git describe --abbrev=0 --tags)
 
 while IFS= read -r afile; do
 	echo "Updated version to $today in file $afile"
-	perl -pi -e "s/\b[0-9]{4}\.[12]?[0-9]\.[12]?[0-9]\b/$today/g" "$afile"
+	perl -pi -e "s/\b[0-9]{4}\.[12]?[0-9]\.[123]?[0-9]\b/$today/g" "$afile"
 	git add "$afile"
-done < <(pcregrep -lwr "\d{4}\.[12]?[0-9]\.[12]?[0-9]" ./*/*.py pyproject.toml)
+done < <(pcregrep -lwr "\d{4}\.[12]?[0-9]\.[123]?[0-9]" ./*/*.py pyproject.toml)
 
 while IFS= read -r afile; do
 	echo "Updated last_tag to $last_tag in file $afile"
-	perl -pi -e "s/\b[0-9]{4}\.[12]?[0-9]\.[12]?[0-9]\b/$last_tag/g" "$afile"
+	perl -pi -e "s/\b[0-9]{4}\.[12]?[0-9]\.[123]?[0-9]\b/$last_tag/g" "$afile"
 	git add "$afile"
-done < <(pcregrep -lwr "\d{4}\.[12]?[0-9]\.[12]?[0-9]" README.md)
+done < <(pcregrep -lwr "\d{4}\.[12]?[0-9]\.[123]?[0-9]" README.md)
 
 exit 0
