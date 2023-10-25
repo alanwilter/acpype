7c22457b6d0e37b6a8e046241ace14079793502f
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2023-10-25 19:47:53 +0100

    Docs again

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ version = release
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 
 autosummary_generate = True
 
@@ -57,7 +57,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"  # "classic"
+# html_theme = "sphinx_rtd_theme"  # "classic"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
