2a3a4cedac3f2774a4d4917af98caba3be00cba3
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2023-10-25 19:44:15 +0100

    Troubleshooting docs

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ version = release
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.autosummary"]
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinx_rtd_theme"]
 
 autosummary_generate = True
 
@@ -62,4 +62,4 @@ html_theme = "sphinx_rtd_theme"  # "classic"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
